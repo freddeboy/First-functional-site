feb52a58873e831e8eca003ac00762a6273a0803
Solider47 <solider.4729@gmail.com>
2024-10-12 17:43:04 +0200

    Fixed all the names to be index and changed java to work

--- Search-Links/12 project thing (Just gonna do 1)/script.js
+++ Search-Links/12 project thing (Just gonna do 1)/script.js
@@ -29,7 +29,7 @@ document.getElementById('searchInput').addEventListener('keypress', function(eve
                 case 'anime rankings':
                 case 'animerankings':
                 case 'anime':
-                    window.location.href = 'Search-Links/Anime-Search/Testing.html';
+                    window.location.href = 'Search-Links/Anime-Search/index.html';
                     break;
                 case 'Cook':
                 case 'Cookbook':
